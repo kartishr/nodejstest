aa0c1ca6b87c04158259851b546c3379f441479d
kalyan <kreddy@ganakalabs.com>
2020-12-17 14:26:35 +0000

    first nodejs commit

---
+++ index.js
@@ -0,0 +1,8 @@
+ex`ports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
